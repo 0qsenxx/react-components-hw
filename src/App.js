cf0379e3079e4736ef8c79d4a0f5db5da3bbb6f3
0qsenxx <semen.antipyuk@gmail.com>
2024-07-02 20:52:03 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
     <>
       <Greeting name="Semen" />
       <Message text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi orci odio, porta sit amet condimentum vel, mollis sed sem. Quisque laoreet elit nec risus finibus, et auctor velit finibus." />
-      <Button onClick={onClick} />
+      <Button onClick={() => onClick('hello')} />
     </>
   );
 }
 
-function onClick() {
-  console.log("success click");
+function onClick(message) {
+  console.log(message);
 }
 
 export default App;
